5937c527b2f521c34fd9febd1ffb3f9c252a15f1
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-07 18:14:26 +0900

    Initial commit
    Co-authored-by: carlosiusalazar <carlosiusalazar@users.noreply.github.com>
    Co-authored-by: yasuhiko-nara <yasuhiko-nara@users.noreply.github.com>

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,6 +1,61 @@
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
 // your class here
 /*
   >>> Don't forget to use module.exports!
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+class VendingMachine {
+  constructor() {
+    this.balance = 0;
+    this.till = {
+      "10": 0,
+      "50": 0,
+      "100": 0,
+      "500": 0,
+    };
+    this.selected = {
+      selectedRow: undefined,
+      selectedColumn: undefined,
+    };
+    this.inventory;
+  }
+  insertCoin(insertedCoin) {
+    this.balance += insertedCoin;
+    this.till[`${insertedCoin}`]++;
+  }
+  selectRow(row) {
+    this.selected.selectedRow = row;
+  }
+  selectColumn(column) {
+    this.selected.selectedColumn = column;
+  }
+  purchase() {
+    const drink = this.inventory[this.selected.selectedRow][
+      this.selected.selectedColumn
+    ];
+    if (drink.count > 0 || this.balance > drink.price) {
+      console.log("CCCCCCCCCCCC", drink.count);
+      console.log(
+        "DDDDDDDDDD",
+        this.selected.selectedRow,
+        this.selected.selectedColumn
+      );
+      if (
+        this.selected.selectedRow !== undefined &&
+        this.selected.selectedColumn !== undefined
+      ) {
+        this.balance -= drink.price;
+        console.log("AAAAAAAAAAA", drink.count);
+        drink.count--;
+        console.log("BBBBBBBBBB", drink.count);
+        console.log(`HERE is the ${drink.name}`);
+      }
+      this.selected.selectedRow = undefined;
+      this.selected.selectedColumn = undefined;
+    } else console.log("DDDDDDDDD sold out or lack of balance");
+  }
+}
+
+module.exports = VendingMachine;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
-  it("should accept valid coins", () => {
+  it("#1 should accept valid coins", () => {
     // Setup
     const machine = new VendingMachine();
 
@@ -17,5 +18,44 @@ describe("vending machine", () => {
       500: 1,
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
+  });
+  it("#2 should save selected row, and log it to the console", () => {
+    // Setup
+    const machine = new VendingMachine();
+
+    // Exercise
+    machine.insertCoin(500);
+    machine.selectRow(0);
+    // console.log("AAAAAAAAAAA", machine.selected.row);
+
+    // Assert
+    expect(machine.selected.selectedRow).to.be.equal(0);
+  });
+
+  it("#3 should log row and column if row is selected and sufficient balance, and sufficient inventory and column is selected", () => {
+    // Setup
+    const machine = new VendingMachine();
+
+    // Exercise
+    const juice = { name: `Apple Juice`, price: 350, count: 5 };
+    const coffee = { name: "Tully's", price: 250, count: 7 };
+
+    const inventory = [
+      [juice, coffee, coffee, coffee],
+      [coffee, coffee, coffee, coffee],
+      [coffee, coffee, coffee, coffee],
+    ];
+    machine.inventory = inventory;
+    machine.insertCoin(500);
+    machine.selectRow(0);
+    machine.selectColumn(0);
+    machine.purchase();
+    //console.log("AAAAAAAAAAA", machine.selected.row);
+
+    // Assert
+    expect(machine.inventory[0][0].count).to.be.equal(4);
+    expect(machine.balance).to.be.equal(150);
+    expect(machine.selected.selectedRow).to.equal(undefined);
+    expect(machine.selected.selectedColumn).to.equal(undefined);
   });
 });
